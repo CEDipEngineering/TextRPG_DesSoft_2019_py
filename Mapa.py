3c35676e94e4eeaae873f85ab58ce17a3a577a49
CEDipEngineering <49285328+CEDipEngineering@users.noreply.github.com>
2019-04-11 15:00:52 -0300

    Add files via upload

---
+++ Mapa.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  8 09:51:49 2019
+Fazer um jogo de RPG de texto estilo anos 80
+@author: Carlos Dip
+"""
+#Mapa onde o jogo acontece. Fácilmente expandido,
+#formato:
+#Mapa={
+# 'Nome do lugar': (
+#   '''Descrição do lugar, e de algumas possiveis
+#   opcoes''',
+#   [opções]
+#)
+#}
+
+
+Mapa = {
+'CalabouçoInicial': (
+        'Carai',
+        ['cima','baixo']
+    ),
+'Calabouço': (
+        'Carai',
+        ['cima','baixo']
+    ),
+'Calabouço2': (
+        'Carai',
+        ['cima','baixo']
+    ),
+'Calabouço3': (
+        'Carai',
+        ['cima','baixo']
+    ),
+'Calabouço4': (
+        'Carai',
+        ['cima','baixo']
+    ),
+'Calabouço5': (
+        'Carai',
+        ['cima','baixo']
+    ),
+'Calabouço6': (
+        'Carai',
+        ['cima','baixo']
+    ),
+'Calabouço7': (
+        'Carai',
+        ['cima','baixo']
+    ),
+}
+print(Mapa)
+
+
